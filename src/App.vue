4eb247b006277a2f01424d4e01b65af0962fae13
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-31 19:17:41 +0200

    [UPD] Created scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script>
+export default{
+
+}
 
 </script>
 
 <template>
- 
+ <header>
+
+ </header>
+ <main>
+
+ </main>
 </template>
 
-<style>
-
+<style lang="scss">
+@use './styles/generals.scss'
 </style>
