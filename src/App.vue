13a2a0b331963cdab9cd0ee399115517ee0a7670
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-31 19:18:45 +0200

    [UPD] Small fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 <script>
+import { store } from './store.js';
+import { axios } from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default{
-
+  component:{
+    AppHeader,
+    AppMain,
+  },
+  created() {
+    
+  },
+  methods: {
+    getCardsList(){
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then((result) =>{
+        store.cardsList = result.data;
+      });
+    }
+  },
+  data() {
+    return {
+      store,
+    }
+  },
 }
 
 </script>
@@ -15,5 +37,5 @@ export default{
 </template>
 
 <style lang="scss">
-@use './styles/generals.scss'
+@import './styles/generals.scss'
 </style>
